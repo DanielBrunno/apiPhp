f212df2b4b89f22fce53309401410ec2b28884c6
Daniel Brunno <danielbrunno1@gmail.com>
2020-06-13 13:39:59 -0300

    Funcao Acesso ao banco online

--- lib/Conexao.class.php
+++ lib/Conexao.class.php
@@ -6,15 +6,22 @@
  * @author Daniel Brunno
  */
 class Conexao {
-    //put your code here
+    //put your code here 
 	  
+    private $usuario = "danie932_ativdan";
+    private $senha = "@Ativ123";
+    private $caminho = "http://danielbrunno.com.br/";
+    private $banco = "danie932_atividades";
+    private $con;
+    
+    /*
     private $usuario = "valter";
     private $senha = "@AtivInfor1";
     //private $caminho = "localhost";	
     private $caminho = "127.0.0.1";
     private $banco = "cadastro";
     private $con;
-    
+    */
     public function __construct() {
         $this->con = mysqli_connect($this->caminho, $this->usuario, $this->senha) or die ("Conexao com o banco de dados falhou" . mysqli_error($this->con));
         mysqli_select_db($this->con, $this->banco) or die ("Conexao com o banco de dados falhou" . mysqli_error($this->con));
