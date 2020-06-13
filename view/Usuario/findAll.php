0e29facf9583e22e00faed1864c063dbd32b6900
Daniel Brunno <danielbrunno1@gmail.com>
2020-06-13 19:25:40 -0300

    CRUD Completo2

--- view/Usuario/findAll.php
+++ view/Usuario/findAll.php
@@ -9,7 +9,7 @@ echo "Nome: <br><br> ";
 for($i = 0; $i < count($data); $i++){
 $nome = $data[$i]["nome"];    
 $contato = $data[$i]["contato"];    
-echo "" . $nome . " - " . $contato . " <a style='text-align:left;' href=' https://api.whatsapp.com/send?phone=+55" . $nome. "&text=" . $msn. " '><button style='background: #00FF00; color: #FFFFFF; font-size: 14pt; border-radius: 5px; border-bottom-color: #FFFFFF;'>Enviar Whatssap</button></a> <br><br> ";
+echo "" . $nome . " - " . $contato . " <a style='text-align:left;' href=' https://api.whatsapp.com/send?phone=+55" . $contato. "&text=" . $msn. " '><button style='background: #00FF00; color: #FFFFFF; font-size: 14pt; border-radius: 5px; border-bottom-color: #FFFFFF;'>Enviar Whatssap</button></a> <br><br> ";
 
 }
 
