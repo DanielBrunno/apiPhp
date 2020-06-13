5c829e0de4ef9103f9ad390d9421e4edbacd14ba
Daniel Brunno <danielbrunno1@gmail.com>
2020-06-13 15:26:15 -0300

    Concexao com banco distribuida

--- lib/Conexao.class.php
+++ lib/Conexao.class.php
@@ -7,18 +7,23 @@
  */
 class Conexao {
     //put your code here 
-
     
-    private $usuario = "danie932_ativdan";
+    private $usuario = "consumo_valter";    
+    private $senha = "@Ativ123";
+    private $caminho = "clubedeconsumidores.com.br";
+    private $banco = "consumo_atividades";
+    private $con;
+    
+    /*
+    private $usuario = "danie932_ativdan";    
     private $senha = "@Ativ123";
     private $caminho = "danielbrunno.com.br";
     private $banco = "danie932_atividades";
     private $con;
     
-/*
+  ESTE AQUI SÓ ABILITAR PARA CONEXAO LOCALHOST NO XAMPP
     private $usuario = "valter";
     private $senha = "@AtivInfor1";
-    //private $caminho = "localhost";	
     private $caminho = "127.0.0.1";
     private $banco = "cadastro";
     private $con;
