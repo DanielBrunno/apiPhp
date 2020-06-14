259a71d1f033aee4a5b71190edce503ea3aa1eab
Daniel Brunno <danielbrunno1@gmail.com>
2020-06-14 01:49:44 -0300

    teste banco local 127.0.0.1

--- lib/Conexao.class.php
+++ lib/Conexao.class.php
@@ -8,20 +8,21 @@
 class Conexao {
     //put your code here 
     
-    private $usuario = "consumo_valter";    
+    // BANCO RODANDO ON LINE
+    /* private $usuario = "consumo_valter";    
     private $senha = "@Ativ123";
     private $caminho = "clubedeconsumidores.com.br";
     private $banco = "consumo_atividades";
     private $con;
-    
-/*
-ESTE AQUI SÓ ABILITAR PARA CONEXAO LOCALHOST NO XAMPP
+    */
+
+// BANCO RODANDO LOCALHOST NO XAMPP
     private $usuario = "valter";
     private $senha = "@AtivInfor1";
     private $caminho = "127.0.0.1";
     private $banco = "cadastro";
     private $con;
-    */
+    
     
     public function __construct() {
         $this->con = mysqli_connect($this->caminho, $this->usuario, $this->senha) or die ("Conexao com o banco de dados falhou" . mysqli_error($this->con));
